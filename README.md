df8ef9727fc263d3aeb7713c2fe22f37b2949aed
LeeSeungHyun <iop2480@naver.com>
2023-02-12 20:54:43 +0900

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# google-drive-tree
+# google-drive-tree-server
+google-drive 구조를 트리형태로 보여주기 위한 서버코드 입니다.
--- appsscript/Code.js
+++ appsscript/Code.js
@@ -1,5 +1,23 @@
 function doGet() {
-  return HtmlService.createTemplateFromFile("index")
-      .evaluate()
-      .addMetaTag("viewport", "width=device-width, initial-scale=1.0")
+  // try {
+  //   var parentFolder = DriveApp.getRootFolder();
+  //   getChildFolders(parentFolder);
+  // } catch (e) {
+  //   Logger.log(e.toString());
+  // }
+
+  // return HtmlService.createTemplateFromFile("index")
+  //   .evaluate()
+  //   .addMetaTag("viewport", "width=device-width, initial-scale=1.0")
+  let content = [
+    {
+      name: 'lee',
+      age: 34,
+    },
+    {
+      name: 'joy',
+      age: 25,
+    }
+  ]
+  return ContentService.createTextOutput(JSON.stringify(content) ).setMimeType(ContentService.MimeType.JSON); 
 }
