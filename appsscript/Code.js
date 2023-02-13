4a601b462988fbfddcea8b85f46e498b3c775683
LeeSeungHyun <iop2480@naver.com>
2023-02-13 22:27:22 +0900

    google drive 트리구조 json 형태로 리턴

--- appsscript/Code.js
+++ appsscript/Code.js
@@ -1,23 +1,34 @@
-function doGet() {
-  // try {
-  //   var parentFolder = DriveApp.getRootFolder();
-  //   getChildFolders(parentFolder);
-  // } catch (e) {
-  //   Logger.log(e.toString());
-  // }
+let treeNodes = [];
 
-  // return HtmlService.createTemplateFromFile("index")
-  //   .evaluate()
-  //   .addMetaTag("viewport", "width=device-width, initial-scale=1.0")
-  let content = [
-    {
-      name: 'lee',
-      age: 34,
-    },
-    {
-      name: 'joy',
-      age: 25,
+function doGet() {
+  generateFolderTree();
+  return ContentService
+    .createTextOutput(JSON.stringify(treeNodes.reverse()))
+    .setMimeType(ContentService.MimeType.JSON);
+}
+
+function generateFolderTree() {
+  try {
+    const parentFolder = DriveApp.getRootFolder();
+    getChildFolders(parentFolder, treeNodes);
+  } catch (e) {
+    Logger.log(e.toString());
+  }
+}
+
+function getChildFolders(parent, treeNodes) {
+  const childFolders = parent.getFolders();
+  while (childFolders.hasNext()) {
+
+    const childFolder = childFolders.next();
+    treeNodes.push({ title: childFolder.getName(), children: [] });
+
+    const files = childFolder.getFiles();
+
+    while (files.hasNext()) {
+      treeNodes[treeNodes.length - 1].children.push({title: files.next().getName()});
     }
-  ]
-  return ContentService.createTextOutput(JSON.stringify(content) ).setMimeType(ContentService.MimeType.JSON); 
+
+    getChildFolders(childFolder, treeNodes[treeNodes.length - 1].children);
+  }
 }
